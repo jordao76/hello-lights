0722da5aeed1400760593f019d7bb35f68f8e781
Rodrigo Jordao <rodrigo.jordao@gmail.com>
2018-02-15 16:01:11 -0500

    Make pause cancellable

--- src/commands.js
+++ src/commands.js
@@ -1,5 +1,40 @@
-let pause = ms => new Promise(resolve => setTimeout(resolve, ms));
+class Cancellable {
+  constructor() {
+    this.isCancelled = false;
+    this._timeoutIDs = {};
+  }
+  add(timeoutID, resolve) {
+    this._timeoutIDs[timeoutID.id||timeoutID] = [timeoutID, resolve];
+  }
+  del(timeoutID) {
+    delete this._timeoutIDs[timeoutID.id||timeoutID];
+  }
+  cancel() {
+    this.isCancelled = true;
+    for (let [_, [timeoutID, resolve]] of Object.entries(this._timeoutIDs)) {
+      this.del(timeoutID);
+      clearTimeout(timeoutID);
+      resolve();
+    }
+  }
+}
+
+let cancellable = new Cancellable;
+
+let cancel = (ct=cancellable) => {
+  ct.cancel();
+  cancellable = new Cancellable;
+};
+
+let pause = (ms, ct=cancellable) =>
+  new Promise(resolve => {
+    let timeoutID = setTimeout(() => {
+      ct.del(timeoutID);
+      resolve();
+    }, ms)
+    ct.add(timeoutID, resolve);
+  });
 
 module.exports = {
-  pause
+  Cancellable, cancel, pause
 }
--- test/test-commands.js
+++ test/test-commands.js
@@ -21,6 +21,19 @@ describe('Commands', () => {
         this.clock.tick(510);
     });
 
+    it('should be cancellable', (done) => {
+      let paused = false;
+      let run = async () => {
+        await c.pause(5000);
+        paused = true;
+        done();
+      };
+      run();
+      paused.should.be.false;
+      this.clock.tick(4000);
+      c.cancel();
+    });
+
   });
 
 });
