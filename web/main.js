b16ab4b4a4cb0602f2cae676826f24ed01fcf47b
Rodrigo Jordao <rodrigo.jordao@gmail.com>
2018-02-17 11:54:19 -0500

    Parse commands in web playground

--- web/main.js
+++ web/main.js
@@ -2,13 +2,10 @@
 
 ///////////////
 
-function transfer(s,d) {
-  for (let [k,v] of Object.entries(s)) {
-    d[k]=v;
-  }
-}
-transfer(require('../src/traffic-light.js'),window);
-transfer(require('../src/commands.js'),window);
+var trafficlight = require('../src/traffic-light.js');
+var commands = require('../src/commands.js');
+var Cancellable = require('../src/cancellable.js');
+var CommandParser = require('../src/command-parser.js');
 
 ///////////////
 
@@ -21,12 +18,31 @@ class WebLight {
   toggle() { this.$light.toggleClass('on'); }
 }
 
+var cancellable = new Cancellable;
+window.cancel = function() {
+  cancellable.cancel();
+  cancellable = new Cancellable;
+}
+
+var cp = new CommandParser(commands);
+window.execute = async function(str) {
+  cancel();
+  var command = cp.parse(str);
+  console.log(`Executing command '${str}'`);
+  var res = await commands.run(command,tl,cancellable);
+  if (res instanceof Error) {
+    console.error(`Error executing command '${str}'`);
+    console.error(res.toString());
+  }
+  return res;
+}
+
 ///////////////
 
 $(() => {
   var r = new WebLight('#tl > .red');
   var y = new WebLight('#tl > .yellow');
   var g = new WebLight('#tl > .green');
-  window.tl = new TrafficLight(r,y,g);
-  cycle(tl, 150);
+  window.tl = new trafficlight.TrafficLight(r,y,g);
+  execute('cycle 150 3');
 });
