79b37aa5b624e96d7dced8796e3b90723da93886
Rodrigo Jordao <rodrigo.jordao@gmail.com>
2018-09-11 21:01:45 -0400

    User friendly web interface

--- web/main.js
+++ web/main.js
@@ -26,67 +26,114 @@ class WebLight extends trafficlight.Light {
 
 ///////////////
 
-window.cancel = function() {
-  cp.cancel();
-};
+function info(str) {
+  console.log(str);
+}
+
+function error(str) {
+  let divError = document.querySelector('.error');
+  let spanError = document.querySelector('#error');
+  spanError.innerText = str;
+  divError.style.display = '';
+  console.error(str);
+}
+
+function clearError() {
+  let divError = document.querySelector('.error');
+  let spanError = document.querySelector('#error');
+  spanError.innerText = '';
+  divError.style.display = 'none';
+}
+
+///////////////
 
 var cp = new CommandParser(); defineCommands(cp);
-window.execute = async function(str, shouldCancel = true) {
-  if (shouldCancel) window.cancel();
-  console.log(`Executing command '${str}'`);
-  try {
-    return await cp.execute(str, window.tl);
-  } catch (e) {
-    console.error(`Error executing command '${str}'`);
-    console.error(e.toString());
-  }
-};
 
-window.help = function(commandName) {
-  if (commandName === undefined) {
-    var commandList = cp.commandList.map(c => `    ${c}`);
-    console.log([
-      `Commands for the traffic light`,
-      `> help()`,
-      `> help('command name')`,
-      `> execute('command')`,
-      `> cancel()`,
-      `  available commands:`,
-      ...commandList
-    ].join('\n'));
-  } else {
-    var command = cp.commands[commandName];
-    if (command === undefined) {
-      window.help();
-      return;
+///////////////
+
+async function execute(commandStr) {
+  clearError();
+  cp.cancel();
+  info(`Executing command '${commandStr}'`);
+  try {
+    await cp.execute(commandStr, window.tl);
+    info(`Finished command '${commandStr}'`);
+    if (commandStr.match(/define/)) {
+      showHelp();
     }
-    let usage = (c) => `${c.doc.name} ${c.paramNames.map(n => ':'+n).join(' ')}`;
-    console.log([
-      usage(command),
-      command.doc.desc
-    ].join('\n'));
+  } catch (e) {
+    error(`Error executing command.\n${e}`);
   }
 };
 
-function utter(str) {
-  try {
-    var synth = window.speechSynthesis;
-    var utterance = new SpeechSynthesisUtterance(str);
-    synth.speak(utterance);
-  } catch (e) {}
+///////////////
+
+function showHelp() {
+  let divHelp = document.querySelector('#help');
+  divHelp.innerHTML = '<h2>Commands</h2>';
+  for (let i = 0; i < cp.commandList.length; ++i) {
+    let commandName = cp.commandList[i];
+    let command = cp.commands[commandName];
+    let usage = (c) => `<hr /><h3><code>${c.doc.name} ${c.paramNames.map(n => ':'+n).join(' ')}</code></h3>`;
+    divHelp.innerHTML += [
+      usage(command),
+      command.doc.desc.replace(/:(\s*\(.+\)\s*)$/, `:<br /><code><a href="#top" class="sample">$1</a></code>`)
+    ].join('');
+  }
+  divHelp.innerHTML += '<hr />';
+  setUpSamples();
+};
+
+///////////////
+
+function setUpButtons() {
+  let txtCommand = document.querySelector('#command');
+  let btnRun = document.querySelector('#run');
+  btnRun.addEventListener('click', () => execute(txtCommand.value));
+
+  let btnCancel = document.querySelector('#cancel');
+  btnCancel.addEventListener('click', () => cp.cancel());
+
+  let btnReset = document.querySelector('#reset');
+  btnReset.addEventListener('click', () => execute('reset'));
+}
+
+///////////////
+
+function setUpSamples() {
+  let txtSamples = document.querySelectorAll('.sample');
+  txtSamples.forEach(txtSample =>
+    txtSample.addEventListener('click', () => runCommand(txtSample.innerText)));
+}
+
+///////////////
+
+function runCommand(command) {
+  execute('reset');
+  let txtCommand = document.querySelector('#command');
+  txtCommand.value = command;
+  execute(command);
+}
+
+///////////////
+
+function setUpTrafficLight() {
+  var r = new WebLight('#tl > .red');
+  var y = new WebLight('#tl > .yellow');
+  var g = new WebLight('#tl > .green');
+  window.tl = new trafficlight.TrafficLight(r, y, g);
 }
 
 ///////////////
 
 async function main() {
-  var r = new WebLight('#tl > .red');
-  var y = new WebLight('#tl > .yellow');
-  var g = new WebLight('#tl > .green');
-  window.tl = new trafficlight.TrafficLight(r, y, g);
-  window.help();
-  utter('Open the java script console');
-  window.execute('danger');
+  setUpTrafficLight();
+  showHelp();
+  setUpButtons();
+  runCommand('pulse green');
 }
+
+///////////////
 
 if (document.readyState !== 'loading') {
   main();
